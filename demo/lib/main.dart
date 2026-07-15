7ab5faa630779986b74bc9ca47a492d6070ae239
Priyanka Dafda <priyankadafda239@gmail.com>
2026-07-15 22:00:33 +0530

    stopwatch

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,74 +1,18 @@
 import 'package:flutter/material.dart';
+import './stopwatch/stopwatch.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const StopWatchRun());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class StopWatchRun extends StatelessWidget {
+  const StopWatchRun({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-              color: Colors.white,
-              fontFamily: 'Roboto',
-            ),
-          ),  
-          backgroundColor: const Color.fromARGB(255, 0, 27, 50),
-          leading: const Icon(Icons.menu),
-        ),
-        body: Center(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Container(
-                  height: 200,
-                  color: const Color.fromARGB(255, 0, 140, 255),
-                  child: ElevatedButton(
-                    onPressed: (){
-                      print('Button Pressed');
-                    }, 
-                    child: const Text('Click Me',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 200,
-                  color: const Color.fromARGB(255, 240, 60, 50),
-                  child: const Center(
-                    child: Text('Welcome to Flutter😁',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 200,
-                  color: const Color.fromARGB(255, 252, 59, 255),
-                  child: const Center(
-                    child: Text('Enjoy your Flutter journey! 😊',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ),
-            ]
-          ),
-        ),
-      ),
+      home: StopWatchExample(),
     );
   }
 }
